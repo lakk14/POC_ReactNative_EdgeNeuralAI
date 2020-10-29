34701b4a1613170fc41bd69527331da4d8a43031
Laukik <laukikkakade8@gmail.com>
2020-10-29 19:43:38 +0530

    location while marking attendance

--- App.js
+++ App.js
@@ -1,10 +1,57 @@
-import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, Alert, Modal, TouchableHighlight, ActivityIndicator, TouchableOpacity } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 
+//create component instead function 
 export default function App() {
+
+
+  //Add import { withNavigation } from 'react-navigation';
+
+
+  // static navigationOptions = ({navigation}) => {
+  // 	const { params = {} } = navigation.state;
+  // 	return{
+  // 		title: "FaceRec",
+  // 		headerStyle: {
+  // 		  backgroundColor: '#0177bc'
+  // 		},
+  // 		headerTitleStyle: {
+  // 			fontSize: 18
+  // 		},
+  // 		headerTintColor: '#fff',
+  // 		headerLeft: () => null,
+  // 		headerRight: () => (
+  // 			<Icon
+  // 				name='power-off'
+  // 				type="font-awesome"
+  // 				onPress={() => params.logout() }
+  // 				size={25}
+  // 				color = '#fff'
+  // 				underlayColor= '#0177bc'
+  // 				iconStyle={{ padding: 15 }}
+  // 			/>
+  // 		)
+  // 	}
+  // }
+
+
+  // _logout = async() => {
+  // 		await AsyncStorage.removeItem('isLoggedIn');
+  // 		this.props.navigation.navigate('Auth');
+  // }
+  // componentDidMount() {
+  // 	const { navigation } = this.props
+  // 	navigation.setParams({
+  // 		logout: this._logout
+  // 	})
+  // }
+
+
+
+
 
   const [pickedImage, setPickedImage] = useState();
 
@@ -16,6 +63,8 @@ export default function App() {
 
   const [statusMsg, setStatusMsg] = useState();
 
+  const [cLocation, setClocation] = useState();
+
   const [imageIcon, setImageIcon] = useState(false);
 
   const [isLoading, setIsLoading] = useState(false);
@@ -23,7 +72,7 @@ export default function App() {
   const [borderColor, setBorderColor] = useState(false);
 
 
-
+  //remove this fn
   const verifyPermissions = async () => {
     const result = await Permissions.askAsync(Permissions.CAMERA, Permissions.CAMERA_ROLL);
     if (result.status !== 'granted') {
@@ -35,6 +84,19 @@ export default function App() {
     }
     return true;
   };
+  //remove fn
+  const verifyLPermissions = async () => {
+    const result = await Permissions.askAsync(Permissions.CAMERA, Permissions.LOCATION);
+    if (result.status !== 'granted') {
+      Alert.alert('Insufficient permissions!',
+        'You need to grant location permissions to use this app.',
+        [{ text: 'Ok' }]
+      );
+      return false;
+    }
+    return true;
+  };
+
   const takeImageHandler = async () => {
     const hasPermission = await verifyPermissions();
     if (!hasPermission) {
@@ -46,11 +108,22 @@ export default function App() {
       quality: 0.3 //ranges between 0-1 
     });
     setPickedImage(image.uri);
-
   };
+
+  const getLocation = async () => {
+    try {
+      const location = await Location.getCurrentPositionAsync({ timeInterval: 3000 });
+      setClocation(location);
+    }
+    catch (e) {
+      Alert.alert('Could not fectch location!', 'Please try again', [{ text: "Ok" }]);
+    }
+  }
 
   const submitImage = async () => {
 
+    getLocation();
+    console.log(cLocation);
     const data = new FormData();
 
     data.append('image', { uri: pickedImage, type: "image/jpg", name: "image" });
@@ -67,26 +140,95 @@ export default function App() {
         response => response.json()
       ).then(
         success => {
-          const result = success.prediction[0].faceid;
-          if (result != null) {
-            setEmpId(success.prediction[0].faceid.empId);
-            setName(success.prediction[0].faceid.fname + ' ' + success.prediction[0].faceid.lname)
-            setStatusMsg('Found');
-            setImageIcon(true);
+          if (success.status_code == 200) {
+
+            var imgCount = 0;
+            var empIds = '';
+            var names = '';
+            var coordinatesGreen = [];
+            var coordinatesRed = [];
+
+            success.prediction.map(key => {
+              if (key.confidence != null) {
+                if (key.confidence > 0.7) {
+                  if (empIds != '' && names != '') {
+                    empIds = empIds + ', ' + key.faceid.empId;
+                    names = names + ', ' + key.faceid.fname + ' ' + key.faceid.lname
+                  } else {
+                    empIds = empIds + key.faceid.empId;
+                    names = names + key.faceid.fname + ' ' + key.faceid.lname
+                  }
+                  imgCount++;
+
+                  coordinatesGreen.push(key.face_coordinates);
+                }
+              } else {
+                imgCount = 0;
+                coordinatesRed.push(key.face_coordinates);
+              }
+            });
+
+            if (imgCount >= 1) {
+
+              const hasLPermission = verifyLPermissions();
+              if (!hasLPermission) {
+                return;
+              }
+
+              let ts = new Date(cLocation.timestamp).toISOString().replace(/T/, ' ').replace(/\..+/, '');
+
+              data.append('trans', '{"txnId": 21, "dvcId": 14224190209830000603, "dvcIp": "127.0.0.1", "punchId": "2", "txnDateTime": "' + ts + '", "mode": "IN", "clientId": "2", "location": {"latitude": "' + cLocation.coords.latitude + '", "longitude":"' + cLocation.coords.longitude + '"}}');
+              console.log(data);
+              const locRes = fetch('https://attendance.edgeneural.ai/payroll-attendance/api/public/index.php/api/transaction_new', {
+                method: 'POST',
+                headers: {
+                  'Content-Type': 'multipart/form-data',
+                  'Accept': 'application/json'
+                },
+                body: data,
+              }).then(
+                response => response.json()
+              ).then(
+                success => { console.log(success) }
+              ).catch(
+                error => console.log(error)
+              );
+
+              setEmpId(empIds);
+              setName(names);
+              setImageIcon(true);
+              setBorderColor(true);
+              setStatusMsg('Found');
+            } else {
+              setEmpId('-');
+              setName('-');
+              setBorderColor(false);
+              setImageIcon(false);
+              setStatusMsg('Not Found');
+            }
             setIsLoading(false);
-            setBorderColor(true);
             setModalVisible(true);
-          } else {
-            setStatusMsg('Not Found');
+
+          } else if (success.status_code == 104) {
+            setStatusMsg(success.error);
             setEmpId('-');
             setName('-');
             setPickedImage();
             setImageIcon(false);
-            setIsLoading(false);
             setBorderColor(false);
+            setIsLoading(false);
+
+            setModalVisible(true);
+          } else {
+            setStatusMsg("Something went wrong, try again!");
+            setEmpId('-');
+            setName('-');
+            setPickedImage();
+            setImageIcon(false);
+            setBorderColor(false);
+            setIsLoading(false);
             setModalVisible(true);
           }
-
         }
 
       ).catch(
@@ -120,6 +262,8 @@ export default function App() {
           {borderColor ? (
             <View style={{ ...styles.ImageModal, borderColor: '#008000' }}>
               <Image style={styles.image} source={{ uri: pickedImage }} />
+              <Text style={styles.modalText}>Employee ID: {empId}</Text>
+              <Text style={styles.modalText}>Name: {name}</Text>
             </View>
           ) : (
               <View style={{
@@ -128,11 +272,6 @@ export default function App() {
                 <Image style={styles.image} source={{ uri: pickedImage }} />
               </View>
             )}
-
-
-          <Text style={styles.modalText}>Employee ID: {empId}</Text>
-          <Text style={styles.modalText}>Name: {name}</Text>
-
 
           <TouchableHighlight
             style={{ ...styles.openButton, backgroundColor: "#2196F3" }}
@@ -184,6 +323,9 @@ export default function App() {
   );
 }
 
+//refer container style from enrollment
+
+//container and container1 from login
 const styles = StyleSheet.create({
   buton1: {
     height: 10,
